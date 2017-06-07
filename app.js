8e87e64ddac5465664d5b3b1e2a967f5709296ef
Br0kensword <longislandhustler@gmail.com>
2017-06-06 22:06:23 -0700

    added call to check account status

--- app.js
+++ app.js
@@ -3,3 +3,21 @@
 var Twitter = require('twitter');
 var config = require('./config.js');
 var T = new Twitter(config);
+
+
+var params = {
+  q: '#nodejs',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en'
+}
+
+
+T.get('account/verify_credentials', function(err, data, response) {
+  if(!err){
+    // This is where the magic will happen
+    console.log(response);
+  } else {
+    console.log(err);
+  }
+})
